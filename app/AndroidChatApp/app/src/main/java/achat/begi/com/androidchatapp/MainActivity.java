9f06402f0d866a5ce70aed189d1d9f87aaa732fc
Begum.Yazici <yazicibegum@gmail.com>
2018-01-16 23:45:09 +0300

    Initilized commited

---
+++ app/AndroidChatApp/app/src/main/java/achat/begi/com/androidchatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package achat.begi.com.androidchatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
