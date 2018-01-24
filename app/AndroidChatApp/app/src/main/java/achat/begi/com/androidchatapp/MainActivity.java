56fb77833f2ddaa4a177922fa1f52162a7d9df66
Begum.Yazici <yazicibegum@gmail.com>
2018-01-25 00:33:47 +0300

    commit

--- app/AndroidChatApp/app/src/main/java/achat/begi/com/androidchatapp/MainActivity.java
+++ app/AndroidChatApp/app/src/main/java/achat/begi/com/androidchatapp/MainActivity.java
@@ -3,11 +3,16 @@ package achat.begi.com.androidchatapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
     }
 }
